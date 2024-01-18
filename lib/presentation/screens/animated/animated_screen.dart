9654ef8a1905446029d1c69fe50490753b7fb850
MiguelHG2351 <miguelhernandezgaitan13@gmail.com>
2024-01-18 06:43:15 -0600

    first animation with animation container

--- lib/presentation/screens/animated/animated_screen.dart
+++ lib/presentation/screens/animated/animated_screen.dart
@@ -7,6 +7,26 @@ class AnimatedScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Animated Container'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.play_arrow_rounded),
+      ),
+      body: Center(
+        child: AnimatedContainer(
+          duration: const Duration(milliseconds: 400),
+          width: 100,
+          height: 100,
+          curve: Curves.easeOutCubic,
+          decoration: BoxDecoration(
+            color: Colors.blue,
+            borderRadius: BorderRadius.circular(10)
+          )
+        ),
+      ),
+    );
   }
 }
