f602082a6ee98f48d6074a009a17985bd8a40e69
MiguelHG2351 <miguelhernandezgaitan13@gmail.com>
2024-01-18 06:57:48 -0600

    end the animated container

--- lib/presentation/screens/animated/animated_screen.dart
+++ lib/presentation/screens/animated/animated_screen.dart
@@ -1,10 +1,39 @@
 import 'package:flutter/material.dart';
+import 'dart:math' show Random;
 
-class AnimatedScreen extends StatelessWidget {
+class AnimatedScreen extends StatefulWidget {
   static String name = 'animated_screen';
   
   const AnimatedScreen({super.key});
 
+  @override
+  State<AnimatedScreen> createState() => _AnimatedScreenState();
+}
+
+class _AnimatedScreenState extends State<AnimatedScreen> {
+  double width = 50;
+  double height = 50;
+  Color color = Colors.indigo;
+  double borderRadius = 10.0;
+  
+  void changeShape() {
+    final random = Random();
+    width = random.nextInt(300) + 120;
+    height = random.nextInt(300) + 120;
+    borderRadius = random.nextInt(50) * 20;
+
+    color = Color.fromRGBO(
+      random.nextInt(255),
+      random.nextInt(255),
+      random.nextInt(255),
+      1
+    );
+
+    setState(() {
+      
+    });
+  }
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,18 +41,18 @@ class AnimatedScreen extends StatelessWidget {
         title: const Text('Animated Container'),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: changeShape,
         child: const Icon(Icons.play_arrow_rounded),
       ),
       body: Center(
         child: AnimatedContainer(
           duration: const Duration(milliseconds: 400),
-          width: 100,
-          height: 100,
-          curve: Curves.easeOutCubic,
+          width: width <= 0 ? 50 : width,
+          height: height <= 0 ? 50 : height,
+          curve: Curves.elasticOut,
           decoration: BoxDecoration(
-            color: Colors.blue,
-            borderRadius: BorderRadius.circular(10)
+            color: color,
+            borderRadius: BorderRadius.circular(borderRadius <= 0 ? 10 : borderRadius)
           )
         ),
       ),
