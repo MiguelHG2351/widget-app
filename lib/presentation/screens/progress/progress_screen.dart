8ffa8e8ddd758fcf2836d0d2f97d767d9ae510fd
MiguelHG2351 <miguelhernandezgaitan13@gmail.com>
2024-01-17 22:40:30 -0600

    added snackbar to router and first dialog has been created

--- lib/presentation/screens/progress/progress_screen.dart
+++ lib/presentation/screens/progress/progress_screen.dart
@@ -7,6 +7,66 @@ class ProgressScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Progress indicators'),
+      ),
+      body: const _ProgressView(),
+    );
+  }
+}
+
+class _ProgressView extends StatelessWidget {
+  const _ProgressView();
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Column(
+        children: [
+          SizedBox(height: 30,),
+          Text('Circula progress indicator'),
+          SizedBox(height: 30,),
+          CircularProgressIndicator(strokeWidth: 2, backgroundColor: Colors.black45,),
+          SizedBox(height: 30,),
+          Text('Circula y linear controlado'),
+          _ControllerProgressIndicator()
+        ],
+      )
+    );
+  }
+}
+
+class _ControllerProgressIndicator extends StatelessWidget {
+  const _ControllerProgressIndicator();
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: Stream.periodic( const Duration(milliseconds: 300), (value) {
+        return (value * 2) / 100;
+      }).takeWhile((value) => value < 1),
+      builder: (context, snapshot) {
+        final progressValue = snapshot.data ?? 0;
+        
+        return Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircularProgressIndicator(
+                strokeWidth: 2,
+                value: progressValue,
+                backgroundColor: Colors.black12,
+              ),
+              const SizedBox(width: 20,),
+              Expanded(
+                child: LinearProgressIndicator(value: progressValue,)
+              )
+            ],
+          ),
+        );
+      }
+    );
   }
 }
--- lib/presentation/screens/snackbar/snackbar_screen.dart
+++ lib/presentation/screens/snackbar/snackbar_screen.dart
@@ -5,8 +5,50 @@ class SnackbarScreen extends StatelessWidget {
   
   const SnackbarScreen({super.key});
 
+  void showCustomSnackbar( BuildContext context ) {
+    final snackBar = SnackBar(
+      content: const Text('Hola, soy un snackbar'),
+      action: SnackBarAction(label: 'Ok!', onPressed: () {},),
+      duration: const Duration( seconds: 2 ),
+    );
+
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Snackbar y diálogos'),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () => showCustomSnackbar(context),
+        label: const Text('Mostrar Snackbar'),
+        icon: const Icon(Icons.remove_red_eye_outlined),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FilledButton.tonal(
+              onPressed: () {
+                showAboutDialog(
+                  context: context,
+                  children: [
+                    const Text('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl eget ultricies aliquam, nunc nisl aliquet nunc, quis aliquam nisl nunc sit amet nisl.')
+                  ]
+                );
+              },
+              child: const Text('Licencias usadas')
+            ),
+            FilledButton.tonal(
+              onPressed: () {},
+              child: const Text('Mostrar diálogo')
+            )
+          ],
+        ),
+      )
+    );
   }
 }
