d7bcbdd526ebdbe4817a816e7483c4d2e442c02c
MiguelHG2351 <miguelhernandezgaitan13@gmail.com>
2024-01-19 08:28:08 -0600

    added infinity scroll

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -14,6 +14,7 @@ class HomeScreen extends StatelessWidget {
         title: const Text('Widgets App'),
       ),
       body: const _HomeView(),
+      // drawer: NavigationDrawer(children: []),
     );
   }
 }
