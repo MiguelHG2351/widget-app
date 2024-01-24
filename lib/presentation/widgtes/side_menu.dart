3d18e623ffdfe440fcbae7f61b86f7db56cf51a3
MiguelHG2351 <miguelhernandezgaitan13@gmail.com>
2024-01-24 13:21:04 -0600

    funcionality for close side menu when user closed the menu

--- lib/presentation/widgtes/side_menu.dart
+++ lib/presentation/widgtes/side_menu.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:widgets_app/config/theme/menu/menu_item.dart';
 
 class SideMenu extends StatefulWidget {
-  const SideMenu({super.key});
+  final GlobalKey<ScaffoldState> scaffoldKey;
+
+  const SideMenu({
+    super.key,
+    required this.scaffoldKey
+  });
 
   @override
   State<SideMenu> createState() => _SideMenuState();
@@ -19,6 +25,10 @@ class _SideMenuState extends State<SideMenu> {
         setState(() {
           navDrawerIndex = value;
         });
+
+        final menuItem = appMenuItems[value];
+        context.push(menuItem.link);
+        widget.scaffoldKey.currentState?.closeDrawer();
       },
       children: [
         const Padding(
