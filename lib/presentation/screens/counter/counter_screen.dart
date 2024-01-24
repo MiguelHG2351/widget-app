4dd9c42617eb599a4a409ea9058f9ce3554c767e
MiguelHG2351 <miguelhernandezgaitan13@gmail.com>
2024-01-24 15:46:25 -0600

    Add counter provider and theme provider

--- lib/presentation/screens/counter/counter_screen.dart
+++ lib/presentation/screens/counter/counter_screen.dart
@@ -1,24 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:widgets_app/presentation/providers/counter_provider.dart';
+import 'package:widgets_app/presentation/providers/theme_provider.dart';
 
-class CounterScreen extends StatelessWidget {
+class CounterScreen extends ConsumerWidget {
   const CounterScreen({super.key});
   static String name = 'Counter Screen';
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final colors = Theme.of(context).textTheme;
-    
+    final int clickCounter = ref.watch( counterProvider );
+    final bool isDarkMode = ref.watch( isDarkModeProvider );
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Counter screen'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              ref.read(isDarkModeProvider.notifier).update((state) => !state);
+          },
+          icon: Icon(isDarkMode ? Icons.dark_mode_outlined: Icons.light_mode_outlined))
+        ],
       ),
       body: Center(
-        child: Text('Valor: 10', style: colors.headlineLarge,)
+        child: Text('Valor: $clickCounter', style: colors.headlineLarge,)
       ),
       floatingActionButton: FloatingActionButton(
-        
         onPressed: () {
-          
+          ref.read( counterProvider.notifier ).state++;
         },
         child: const Icon(Icons.add),
       ),
