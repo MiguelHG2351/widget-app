ff90dd7b1583630df3d6582f1acffd9c3e43464a
MiguelHG2351 <miguelhernandezgaitan13@gmail.com>
2024-01-24 13:08:01 -0600

    added list of navigation in the drawer using appMenuItems

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:widgets_app/config/theme/menu/menu_item.dart';
+import 'package:widgets_app/presentation/widgtes/side_menu.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -14,7 +15,7 @@ class HomeScreen extends StatelessWidget {
         title: const Text('Widgets App'),
       ),
       body: const _HomeView(),
-      // drawer: NavigationDrawer(children: []),
+      drawer: const SideMenu(),
     );
   }
 }
